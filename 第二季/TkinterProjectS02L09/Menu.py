6d00dc826ed483af6a8eb3ca3afc8f181d6cec2c
shuiyue-xiaoxiao <373533966@qq.com>
2020-02-26 21:50:07 +0800

    menu

---
+++ 第二季/TkinterProjectS02L09/Menu.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+window = Tk()
+
+window.title('Menu demo')
+
+window.geometry('400x300')
+
+menu_all = Menu(window)
+
+menu_file = Menu(menu_all,
+                 tearoff=0)
+menu_all.add_cascade(label='file',
+                     menu=menu_file)
+menu_file.add_command(label='New')
+menu_file.add_command(label='Open')
+menu_file.add_command(label='Save')
+menu_file.add_separator()
+menu_file.add_command(label='Exit')
+
+menu_help = Menu(menu_all,
+                 tearoff=0)
+menu_all.add_cascade(label='Help',
+                     menu=menu_file)
+menu_help.add_command(label='Help')
+menu_help.add_command(label='About')
+
+window.config(menu=menu_all)
+
+window.mainloop()
