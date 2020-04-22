f464c0e352a8b7fb722229b903b73b66d7d59593
shuiyue-xiaoxiao <373533966@qq.com>
2020-04-22 22:48:58 +0800

    五角星

---
+++ season3/week2/L1/star.py
@@ -0,0 +1,22 @@
+from turtle import *
+
+
+setup(1200, 800)
+hideturtle()
+# penup()
+# goto(-360, 160)
+# pendown()
+speed(5)
+pencolor('black')
+fillcolor('yellow')
+
+begin_fill()
+for i in range(5):
+    forward(70)
+    left(72)
+    forward(70)
+    right(144)
+
+
+end_fill()
+done()
